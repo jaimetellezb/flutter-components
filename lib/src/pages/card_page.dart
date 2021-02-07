29ec9928b04675f06da798dcee2a02a599509362
Jaime Alberto Tellez Bohorquez <jtelle5@bancodebogota.com>
2021-02-07 18:43:01 -0500

    se agrega imagen a una card

--- lib/src/pages/card_page.dart
+++ lib/src/pages/card_page.dart
@@ -13,7 +13,13 @@ class CardPage extends StatelessWidget {
         //   vertical: 20.0
         // ),
         padding: EdgeInsets.all(10.0),
-        children: [_cardType1()],
+        children: [
+          _cardType1(),
+          SizedBox(
+            height: 30.0,
+          ),
+          _cardType2(),
+        ],
       ),
     );
   }
@@ -23,22 +29,50 @@ class CardPage extends StatelessWidget {
       child: Column(
         children: [
           ListTile(
-            leading: Icon(Icons.photo_album, color: Colors.blue,),
+            leading: Icon(
+              Icons.photo_album,
+              color: Colors.blue,
+            ),
             title: Text('Título de la tarjeta'),
-            subtitle: Text('Aquí está la descripción de la tarjeta que indica a qué se refiere la misma.'),
+            subtitle: Text(
+                'Aquí está la descripción de la tarjeta que indica a qué se refiere la misma.'),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
               FlatButton(
                 child: Text('Cancelar'),
-                onPressed: (){},
+                onPressed: () {},
               ),
               FlatButton(
                 child: Text('Ok'),
-                onPressed: (){},
+                onPressed: () {},
               )
             ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _cardType2() {
+    return Card(
+      child: Column(
+        children: [
+          FadeInImage(
+            image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/c/c8/Untersberg_Mountain_Salzburg_Austria_Landscape_Photography_%28256594075%29.jpeg'),
+            placeholder: AssetImage('assets/jar-loading.gif'),
+            fadeInDuration: Duration(milliseconds: 200),
+            height: 250.0, // para que no se mueva la tarjeta
+            fit: BoxFit.cover,
+          ),
+          // Image(
+          //   // imágenes de la web
+          //   image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/c/c8/Untersberg_Mountain_Salzburg_Austria_Landscape_Photography_%28256594075%29.jpeg'),
+          // ),
+          Container(
+            padding: EdgeInsets.all(10.0),
+            child: Text('No se que poner'),
           )
         ],
       ),
