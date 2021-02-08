dccf765fecacd7a8222ab80c84e35b8ca2a2952f
Jaime Alberto Tellez Bohorquez <jtelle5@bancodebogota.com>
2021-02-07 19:52:08 -0500

    Se agregan avatars

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -7,6 +7,26 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: [
+          CircleAvatar(
+            backgroundImage: NetworkImage('https://s-media-cache-ak0.pinimg.com/originals/29/88/ab/2988ab54d9df4bd3b66f2486cdc93ad8.jpg'),
+           ),
+          Container(
+            padding: EdgeInsets.all(4.0),
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('MB'),
+              backgroundColor: Colors.brown,
+            ),
+          )
+        ],
+      ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://assets.tonica.la/__export/1609884639754/sites/debate/img/2021/01/05/rowan-atkinson-ya-no-disfruta-al-interpretar-a-mr-bean.jpg_1037907305.jpg'),
+          placeholder: AssetImage('assets/loading2.gif'),
+          fadeInDuration: Duration(milliseconds: 200),
+        ),
       ),
     );
   }
