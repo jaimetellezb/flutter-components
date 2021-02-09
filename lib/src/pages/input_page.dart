1a810c2ade8ea6686488818e2dc391dd09465822
Jaime Alberto Tellez Bohorquez <jtelle5@bancodebogota.com>
2021-02-08 20:44:21 -0500

    Input field

---
+++ lib/src/pages/input_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  String _name;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Inputos de texto"),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+        children: [_createInput(), Divider(), _createPerson()],
+      ),
+    );
+  }
+
+  Widget _createInput() {
+    return TextField(
+      // autofocus: true,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(20.0)),
+          counter: Text('Letras 0'),
+          hintText: 'Nombre de la persona',
+          labelText: 'Nombre',
+          helperText: 'Sólo es el nombre',
+          suffixIcon: Icon(Icons.accessibility),
+          icon: Icon(Icons.account_circle)),
+      onChanged: (value) {
+        setState(() {
+          _name = value;
+        });
+      },
+    );
+  }
+
+  _createPerson() {
+    return ListTile(
+      title: Text('Nombre es: $_name'),
+    );
+  }
+}
--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -3,6 +3,7 @@ import 'package:components/src/pages/animated_container.dart';
 import 'package:components/src/pages/avatar_page.dart';
 import 'package:components/src/pages/card_page.dart';
 import 'package:components/src/pages/home_page.dart';
+import 'package:components/src/pages/input_page.dart';
 import 'package:flutter/material.dart';
 
 
@@ -12,7 +13,8 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     'alert': (BuildContext context) => AlertPage(),
     'avatar': (BuildContext context) => AvatarPage(),
     'card':  (BuildContext context) => CardPage(),
-    'animatedContainer': (BuildContext context) => AnimatedContainerPage()
+    'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
+    'inputs':  (BuildContext context) => InputPage(),
   };
 }
 
