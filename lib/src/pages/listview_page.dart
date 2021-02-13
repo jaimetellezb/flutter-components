0d254069b569daa1d5bec05920fe247122d6ec3c
Jaime Alberto Tellez Bohorquez <jtelle5@bancodebogota.com>
2021-02-13 15:42:40 -0500

    scroll

--- lib/src/pages/listview_page.dart
+++ lib/src/pages/listview_page.dart
@@ -6,7 +6,25 @@ class ListPage extends StatefulWidget {
 }
 
 class _ListPageState extends State<ListPage> {
-  List<int> _numberList = [1, 2, 3, 4, 5];
+  ScrollController _scrollController = new ScrollController();
+
+  List<int> _numberList = new List();
+  int _lastItem = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _add10();
+
+    _scrollController.addListener(() {
+      // indica que estamos al final de la página
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _add10();
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,15 +37,24 @@ class _ListPageState extends State<ListPage> {
 
   Widget _createList() {
     return ListView.builder(
+      controller: _scrollController,
       itemCount: _numberList.length,
       itemBuilder: (BuildContext context, int index) {
         final image = _numberList[index];
         return FadeInImage(
-          image: NetworkImage(
-              'https://picsum.photos/500/300/?image=$image'),
-              placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https://picsum.photos/500/300/?image=$image'),
+          placeholder: AssetImage('assets/jar-loading.gif'),
         );
       },
     );
+  }
+
+  void _add10() {
+    for (var i = 0; i < 10; i++) {
+      _lastItem++;
+      _numberList.add(_lastItem);
+    }
+
+    setState(() {});
   }
 }
